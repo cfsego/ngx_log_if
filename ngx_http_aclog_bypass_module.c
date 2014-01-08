bc553abb8f55aa88fe191eb8771dc03b65f0c21a
Chuanwen Chen <crk_world@yahoo.com.cn>
2014-01-08 23:17:18 +0800

    validation parent's conf

--- ngx_http_aclog_bypass_module.c
+++ ngx_http_aclog_bypass_module.c
@@ -113,6 +113,15 @@ ngx_http_aclog_bypass_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child)
 
     conf->log_off = lcf->off;
 
+    if (prev->conditions) {
+        condition = prev->conditions->elts;
+        if (condition[conf->conditions->nelts - 1].is_and) {
+            ngx_conf_log_error(NGX_LOG_EMERG, cf, 0,
+                               "can not use \"and\" flag on the last condition");
+            return NGX_CONF_ERROR;
+        }
+    }
+
     if (lcf->off || lcf->logs == NULL) {
         return NGX_CONF_OK;
     }
